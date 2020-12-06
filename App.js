a5fdb694b52514fb14db7c11d98f1c8442cab9a7
RealRayD <73535538+RealRayD@users.noreply.github.com>
2020-12-06 20:04:43 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HelloWorldApp = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <Text>Hello World from Group 1 : Reinis Rainers Engers! This is my Reinis Rainers Engers' first React Native application!
+</Text>
+    </View>
+  )
+}
+export default HelloWorldApp;
